4a3687320763371ef08b0a9693a12970811da51d
gilc1870 <gilc1870@wrdsb.ca>
2020-10-14 15:48:39 +0000

    Which numbers go in ascending order

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 /**
+
+
  *
- * @author 
+ * @author Cesar Gil
  */
 public class Main {
 
@@ -9,7 +13,19 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter an integer");
+    int num = input.nextInt();
+    System.out.println("Please enter another integer");
+    int num2 = input.nextInt();
+    // Which numbers go in ascending order
+    if (num > num2){
+      System.out.println("Your numbers in ascending order are " + num2 + "," + num);
+    } else if ( num < num2) {
+      System.out.println("Your numbers in ascending order are " + num + "," + num2);
+    } if (num == num2) {
+      System.out.println("Your numbers in ascending order are " + num + "," + num2);
+
+    }
   }
 }
